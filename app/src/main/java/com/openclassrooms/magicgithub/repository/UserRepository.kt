715e822dd2c93970c4029ac552f00574f59b3d1e
salah0250 <salah-eddine.aboulkacim@etu.univ-cotedazur.fr>
2025-01-06 17:20:41 +0100

    Etape 1: Fixer les tests

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.kt
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.kt
@@ -7,14 +7,15 @@ class UserRepository(
     private val apiService: ApiService
 ) {
     fun getUsers(): List<User> {
-        TODO("Should return the list of users retrieved from the api service")
+        return apiService.getUsers()
     }
 
     fun addRandomUser() {
-        TODO("Should add a random user")
+        apiService.addRandomUser()
     }
 
     fun deleteUser(user: User) {
-        TODO("Should remove the user")
+        apiService.deleteUser(user)
     }
+
 }
